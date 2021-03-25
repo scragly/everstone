9bc3fb324a1b349fdc5d2502536f8638c123a308
scragly <29337040+scragly@users.noreply.github.com>
2021-03-26 09:31:05 +1000

    Add Table.create, Table.add_columns.

--- everstone/sql/table.py
+++ everstone/sql/table.py
@@ -4,6 +4,7 @@ import typing as t
 
 from . import column
 from .. import database
+from ..exceptions import SchemaError
 
 if t.TYPE_CHECKING:
     from .constraints import Constraint
@@ -26,6 +27,8 @@ class Table:
 
         self.schema.add_table(self)
 
+        self.columns: t.Dict[str, column.Column] = dict()
+
     @property
     def full_name(self) -> str:
         """Return the fully qualified name of the current table."""
@@ -42,19 +45,35 @@ class Table:
             return self.full_name == other.full_name
         return False
 
+    def add_columns(self, *columns: column.Column) -> Table:
+        """Add columns to the current table."""
+        for c in columns:
+            c.bind_table(self)
+            self.columns[c.name] = c
+        return self
+
     async def prepare(self):
         """Ensure the table exists in the database."""
-        await self.create(if_exists=False)
+        await self.create(if_not_exists=True)
 
-    async def create(self, if_exists: bool = False):
+    async def create(self, if_not_exists: bool = False) -> str:
         """Create the table in the database."""
-        pass
+        if not self.columns:
+            raise SchemaError("Table creation failed: No columns.")
+        exists = "IF NOT EXISTS " if if_not_exists else ""
+        cols = ", ".join(c.definition for c in self.columns.values())
+        sql = f"CREATE TABLE {exists}{self.name} ({cols});"
+        return await self.db.execute(sql)
 
-    async def drop(self):
-        """Drop table from database."""
-        sql = "DROP TABLE $1"
-        await self.db.execute(sql, self.full_name)
+    async def drop(self, if_exists: bool = False, cascade: bool = False) -> str:
+        """Drop the table from the database."""
+        exists = "IF EXISTS " if if_exists else ""
+        cascade = " CASCADE" if cascade else ""
+        sql = f"DROP TABLE {exists}{self.name}{cascade};"
+        return await self.db.execute(sql)
 
     def Column(self, name: str, type: SQLType, constraint: t.Optional[Constraint] = None) -> Column:
         """Return a Column instance bound to this table."""
-        return column.Column(name, type, constraint).bind_table(self)
+        col = column.Column(name, type, constraint).bind_table(self)
+        self.columns[col.name] = col
+        return col
