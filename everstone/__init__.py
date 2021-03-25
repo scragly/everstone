51bcb836dfb328ed56be188741b1e3327a8444ee
scragly <29337040+scragly@users.noreply.github.com>
2021-03-26 09:35:22 +1000

    Make some base SQL objects accessible from root.

--- everstone/__init__.py
+++ everstone/__init__.py
@@ -1,3 +1,7 @@
 from .database import Database
+from .sql import aggregates, constraints, types
+from .sql.column import Column
+from .sql.schema import Schema
+from .sql.table import Table
 
 db = Database("__default__")
