f5af0c81d0429948ee5962796c6d20fbb9250e0d
scragly <29337040+scragly@users.noreply.github.com>
2021-03-27 18:57:46 +1000

    Columns: Add missing aggregates, add default aliases.

--- everstone/sql/column.py
+++ everstone/sql/column.py
@@ -40,33 +40,61 @@ class Column(comparisons.Comparable):
         self.table = table
         return self
 
+    # region: meta
+
     def __str__(self):
         return self.full_name
 
     def __repr__(self):
         return f"<{self.__class__.__name__} {self.full_name} {self.type}>"
 
+    # endregion
+
+    # region: aggregates
+
     @property
     def avg(self) -> aggregates.Avg:
-        """Return the avg aggreate for this column."""
-        return aggregates.Avg(self)
+        """Return the avg aggregate for this column."""
+        return aggregates.Avg(self).as_("avg")
+
+    @property
+    def bit_and(self) -> aggregates:
+        """Return the bit_and aggregate for this column."""
+        return aggregates.Avg(self).as_("bit_and")
+
+    @property
+    def bit_or(self) -> aggregates:
+        """Return the bit_or aggregate for this column."""
+        return aggregates.Avg(self).as_("bit_or")
+
+    @property
+    def bool_and(self) -> aggregates:
+        """Return the bool_and aggregate for this column."""
+        return aggregates.Avg(self).as_("bool_and")
+
+    @property
+    def bool_or(self) -> aggregates:
+        """Return the bool_or aggregate for this column."""
+        return aggregates.Avg(self).as_("bool_or")
 
     @property
     def count(self) -> aggregates.Count:
-        """Return the count aggreate for this column."""
-        return aggregates.Count(self)
+        """Return the count aggregate for this column."""
+        return aggregates.Count(self).as_("count")
 
     @property
     def max(self) -> aggregates.Max:
-        """Return the max aggreate for this column."""
-        return aggregates.Max(self)
+        """Return the max aggregate for this column."""
+        return aggregates.Max(self).as_("max")
 
     @property
     def min(self) -> aggregates.Min:
-        """Return the min aggreate for this column."""
-        return aggregates.Min(self)
+        """Return the min aggregate for this column."""
+        return aggregates.Min(self).as_("min")
 
     @property
     def sum(self) -> aggregates.Sum:
-        """Return the sum aggreate for this column."""
-        return aggregates.Sum(self)
+        """Return the sum aggregate for this column."""
+        return aggregates.Sum(self).as_("sum")
+
+    # endregion
