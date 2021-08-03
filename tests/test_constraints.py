9beb9f0a724c238037bb5ba6f23444bf8e0c049f
scragly <29337040+scragly@users.noreply.github.com>
2021-08-04 06:14:03 +1000

    Tests: Add last of constraints functionality.

--- tests/test_constraints.py
+++ tests/test_constraints.py
@@ -1,6 +1,8 @@
 """Testing of SQL Constraint functionality."""
 import pytest
 
+import everstone
+from everstone.exceptions import SchemaError
 from everstone.sql import column, constraints, types
 
 
@@ -20,13 +22,18 @@ def test_constraint(unique_instance, unique_class):
     assert repr(unique_instance) == '<Unique sql="UNIQUE">'
     assert unique_instance == constraints.Unique()
     assert unique_instance != constraints.NotNull()
+    assert unique_instance != "randomstring"
     assert unique_instance == unique_class
     assert unique_class.sql == "UNIQUE"
     assert str(unique_class) == "UNIQUE"
     assert repr(unique_class) == '<Unique sql="UNIQUE">'
     assert unique_class == constraints.Unique
     assert unique_class != constraints.NotNull()
+    assert unique_class != "randomstring"
     assert unique_class == unique_instance
+    assert hash(unique_instance) == hash(unique_class)
+    assert constraints.NotNull.sql == "NOT NULL"
+    assert constraints.PrimaryKey.sql == "PRIMARY KEY"
 
 
 def test_constraint_named(unique_instance, unique_class):
@@ -39,3 +46,19 @@ def test_constraint_composite(unique_instance, unique_class):
     assert c.sql == "UNIQUE (test_a, test_b)"
     c = unique_class.columns(column.Column("test_a", types.Text), column.Column("test_b", types.Text))
     assert c.sql == "UNIQUE (test_a, test_b)"
+
+
+def test_constraint_check():
+    c = constraints.Check(column.Column("test_a", types.Integer) > 10)
+    assert c.sql == "CHECK (test_a > 10)"
+    c = constraints.Check(column.Column("test_a", types.Integer) > 10, name="above_ten")
+    assert c.sql == "CONSTRAINT above_ten CHECK (test_a > 10)"
+
+
+def test_constraint_foreign_key():
+    c = constraints.ForeignKey(everstone.db.Table("test_table").Column("col_a", types.Text))
+    assert c.sql == "REFERENCES public.test_table (col_a)"
+    c = constraints.ForeignKey(column.Column("col_a", types.Text), table="test_table")
+    assert c.sql == "REFERENCES test_table (col_a)"
+    with pytest.raises(SchemaError):
+        _ = constraints.ForeignKey(column.Column("col_a", types.Text))
